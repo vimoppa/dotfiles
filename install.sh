0df140288afa11f43532906a35c8ba67dc983d12
Josh <mail@chukwuanu.com>
2021-01-01 22:01:46 +0100

    gitconfig, update install.sh
    * add gitconfig
    * cleanup symlink on install

--- install.sh
+++ install.sh
@@ -10,16 +10,28 @@ fi
 # NOTE: user specific configuration files are in gitignore
 ## [ -e "viminfo" ] && rm "./viminfo" || touch "./viminfo"
 
+function run() {
 
 # symlink dotfiles to $HOME directory
 for f in $USER_WORKDIR/dotfiles/*
 do
-    if [ "${f##*/}" != "install.sh" ] && [ "${f##*/}" != "readme" ]
+    local file="${f##*/}"
+    local file_home="$HOME/.$file"
+
+    if [ "$file" != "install.sh" ] && [ "$file" != "readme" ]
     then
-	    ln -s "$f" "$HOME/.${f##*/}"
+        # cleanup symlink, if existing
+        [ -e "$file_home" ] && {
+            echo "$file link exists, cleaning up..."
+            rm "$file_home"
+        }
+
+        ln -s "$f" "$file_home" && {
+            echo "new '$file_home' linked"
+        }
     fi
 done
 
+}
 
-# setup oh-my-zsh
-sh "$ZSH/tools/install.sh"
+run $@
