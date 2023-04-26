de8608a85edb81211606a65ecf49761367ef9316
Josh <mail@chukwuanu.com>
2023-04-26 21:15:10 +0100

    chore: cleanup install script & gitmodules

--- install.sh
+++ install.sh
@@ -1,90 +1,42 @@
 #!/usr/bin/env bash
 
-if [ -z $USER_WORKDIR ]
-then
-    echo "env => USER_WORKDIR not set: '$USER_WORKDIR' "
-    exit $?
+if [ -z $USER_WORKDIR ]; then
+  echo "env => USER_WORKDIR not set: '$USER_WORKDIR' "
+  exit $?
 fi
 
-# delete and recreate specific user configuration files
-# NOTE: user specific configuration files are in gitignore
-## [ -e "viminfo" ] && rm "./viminfo" || touch "./viminfo"
+# config symlinks are handled separately
+declare dotfiles_files=("bin" "config" "install.sh" "readme")
 
-function command_exists() {
-    command -v "$@" >/dev/null 2>&1
-}
+function setup_config() {
+  # symlink config to $HOME/.config directory
+  for c in $USER_WORKDIR/dotfiles/config/*; do
+    local cfg="${c##*/}"
+    local config="${HOME}/.config/${cfg}"
 
-function setup_zsh() {
-    local ZSH_THEME_DIR="$HOME/.zsh/themes"
-    for z in $ZSH_THEME_DIR/*
-    do
-        local zsh_file="${z##*/}"
-        if [[ $zsh_file == *.zsh-theme ]]
-        then
-            continue
-        fi
+    [ -e "${config}" ] && {
+      echo "${cfg} link exists, cleaning up..."
+      rm -rf "${config}"
+    }
 
-        for i in $z/*
-        do
-            local inner_file="${i##*/}"
-            if [[ $inner_file == *.zsh-theme ]]
-            then
-                if [ -e $ZSH_THEME_DIR/$inner_file ]
-                then
-                    echo "$inner_file exists, cleaning up..."
-                    rm "$ZSH_THEME_DIR/$inner_file"
-                fi
-
-
-                ln -s "$i" "$ZSH_THEME_DIR/$inner_file" && {
-                    echo "new '$z' linked"
-                }
-            fi
-        done
-    done
+    ln -s "${c}" $"${config}" && {
+      echo "new '${config}' linked"
+    }
+  done
 }
 
 function run() {
-    command_exists git || {
-        echo "git is not installed"
-        exit 1
-    }
+  #    command_exists git || {
+  #        echo "git is not installed"
+  #        exit 1
+  #    }
 
-    # symlink dotfiles to $HOME directory
-    for f in $USER_WORKDIR/dotfiles/*
-    do
-        local file="${f##*/}"
-        local file_home="$HOME/.$file"
+  ln -s $USER_WORKDIR/zshrc ~/.zshrc
+  ln -s $USER_WORKDIR/gitconfig ~/.gitconfig
+  ln -s $USER_WORKDIR/git ~/.git
 
-        if [ "$file" != "install.sh" ] && [ "$file" != "readme" ]
-        then
-            # cleanup symlink, if existing
-            [ -e "$file_home" ] && {
-                echo "$file link exists, cleaning up..."
-                rm "$file_home"
-            }
-
-            ln -s "$f" "$file_home" && {
-                echo "new '$file_home' linked"
-            }
-        fi
-    done
-
-    local VIM_START_PLUGIN_DIR="$HOME/.vim/pack/plugins/start"
-
-    for p in $VIM_START_PLUGIN_DIR/*
-    do
-        if [ -d "$p/doc" ]
-        then
-	    echo "-> ${p##*/}: Loading docs"
-            vim -u NONE -c "helptags $p/doc" -c q
-	else
-	    echo "-> ${p##*/}: No docs loaded"
-        fi
-    done
-
-    # setup zsh plugin, theme
-    setup_zsh
+  # setup config symlinks
+  setup_config
 }
 
 run $@
